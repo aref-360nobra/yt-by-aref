ad45dbee32548f45c1defb7b563fa73165017ad3
aref-360nobra <aref0171personal@gmail.com>
2024-06-22 21:38:43 +0600

    small device blocked

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth < 768) {
+        alert("This website is not supported on small screens.");
+    }
+});
